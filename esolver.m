41ad3bdb660c3e635a26999f3e9dd977537fa52b
Paweł A. Ryszawa <pablo53@poczta.onet.eu>
2026-05-29 11:57:54 +0200

    improved solver

--- esolver.m
+++ esolver.m
@@ -1,11 +1,14 @@
-function [x, y] = esolver(x0, f, step, maxsteps, maxgranularity)
+function [x, y] = esolver(x0, f, islegal, step, maxsteps, maxgranularity)
   if (nargin < 3)
-    step = 1;
+    islegal = @(x) true;
   endif;
   if (nargin < 4)
-    maxsteps = round(1000000 / step);
+    step = 1;
   endif;
   if (nargin < 5)
+    maxsteps = round(1000000 / step);
+  endif;
+  if (nargin < 6)
     maxgranularity = 16;
   endif;
   x = x0;
@@ -18,17 +21,21 @@ function [x, y] = esolver(x0, f, step, maxsteps, maxgranularity)
       xf = x;
       xb(i) = xb(i) - step;
       xf(i) = xf(i) + step;
-      yb = f(xb);
-      yf = f(xf);
-      if (yb < y)
-        y = yb;
-        x = xb;
-        improved = true;
+      if (islegal(xb))
+        yb = f(xb);
+        if (yb < y)
+          y = yb;
+          x = xb;
+          improved = true;
+        endif;
       endif;
-      if (yf < y)
-        y = yf;
-        x = xf;
-        improved = true;
+      if (islegal(xf))
+        yf = f(xf);
+        if (yf < y)
+          y = yf;
+          x = xf;
+          improved = true;
+        endif;
       endif;
     endfor;
     maxsteps = maxsteps - 1;
